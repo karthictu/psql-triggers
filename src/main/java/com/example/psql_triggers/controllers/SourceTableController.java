20388fe653acf8118d8935d2f21ba9dc0714b16c
Karthic Tantry <karthic.spikers@gmail.com>
2025-10-31 14:02:26 +0530

    Remove unused imports

--- src/main/java/com/example/psql_triggers/controllers/SourceTableController.java
+++ src/main/java/com/example/psql_triggers/controllers/SourceTableController.java
@@ -1,7 +1,6 @@
 package com.example.psql_triggers.controllers;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.psql_triggers.entities.SourceTable;
